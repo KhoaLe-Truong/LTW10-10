56694d83272ef72a60c3ea3f999d8fc5d95b449a
KhoaLe-Truong <letruongkhoa99@gmail.com>
2024-10-10 20:42:10 +0700

    BT

--- src/main/java/vn/iotstar/dao/IVideoDao.java
+++ src/main/java/vn/iotstar/dao/IVideoDao.java
@@ -1,5 +1,13 @@
 package vn.iotstar.dao;
 
-public interface IVideoDao {
+import java.util.List;
 
+import vn.iotstar.entity.Video;
+
+public interface IVideoDao {
+	List<Video> findAll();
+    Video findById(int id);
+    void insert(Video video);
+    void update(Video video);
+    void delete(int id);
 }
--- src/main/java/vn/iotstar/services/impl/VideoServices.java
+++ src/main/java/vn/iotstar/services/impl/VideoServices.java
@@ -1,5 +1,12 @@
 package vn.iotstar.services.impl;
 
+import vn.iotstar.entity.Video;
+
 public class VideoServices {
+	List<Video> findAll();
+    Video findById(int id);
+    void insert(Video video);
+    void update(Video video);
+    void delete(int id);
 
 }
